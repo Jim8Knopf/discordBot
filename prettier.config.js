c9c076f9352be1f3540aca622b32811066ab4d54
jkol <jkol@camelot-itlab.com>
2020-08-27 09:07:23 +0200

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	quoteProps: "as-needed",
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: "always",
+	overrides: [
+		{
+			files: ".editorconfig",
+			options: {
+				parser: "yaml",
+			},
+		},
+		{
+			files: "LICENSE",
+			options: {
+				parser: "markdown",
+			},
+		},
+	],
+};
